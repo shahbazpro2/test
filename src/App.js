95a446692c43739fc8f6ffa328a2f4995028f5d1
shahbaz <shahbazrock0321@gmail.com>
2021-07-28 19:58:51 +0500

    add padding on top of postList component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import PostsList from './pages/PostsList';
 function App() {
   return (
-    <div className="App">
+    <div className="App pt-5">
      <PostsList />
     </div>
   );
