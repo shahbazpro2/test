166c02ada0e3da93fa1fd0292f8d58e85b5f9eb2
shahbaz <shahbazrock0321@gmail.com>
2021-07-28 21:13:55 +0500

    add request code

---
+++ src/redux/actions/actionFunctions/GetAllPosts.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { apiUrl } from '../../config/url';
+import * as types from './types'
+
+const getAllPosts = () => {
+    return (dispatch) => { 
+      dispatch({ type: types.GET_ALL_POSTS });
+      return axios.get(`${apiUrl}/posts`).then(  
+        posts => dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts }),
+        err => dispatch({ type: types.GET_ALL_POSTS_FAILURE, err })
+      );
+    };
+  };
+
+export default getAllPosts
