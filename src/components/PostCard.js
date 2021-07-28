4ee9d63359de7df3dd0a8013213ebe992bcc871f
shahbaz <shahbazrock0321@gmail.com>
2021-07-28 19:51:51 +0500

    add dummy post card data

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const PostCard = () => {
     return (
-        <div>
-            
-        </div>
+        <div className="card">
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="/" className="btn btn-primary">see more</a>
+  </div>
+</div>
     )
 }
 
