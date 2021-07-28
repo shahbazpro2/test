044b1177943bc91ecabdac6072f839256443d0f0
shahbaz <shahbazrock0321@gmail.com>
2021-07-28 19:57:42 +0500

    import and use postList component to app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import PostsList from './pages/PostsList';
 function App() {
   return (
     <div className="App">
-     
+     <PostsList />
     </div>
   );
 }
