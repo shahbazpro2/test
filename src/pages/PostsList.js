f42c708c8cb92f477a52888a83291eaf90a3f84a
shahbaz <shahbazrock0321@gmail.com>
2021-07-28 19:56:45 +0500

    import postCard.js component to postList file

--- src/pages/PostsList.js
+++ src/pages/PostsList.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import PostCard from '../components/PostCard'
 
 const PostsList = () => {
     return (
         <div className="container">
-            
+            <div className="row">
+               <div className="col-md-4">
+                    <PostCard />
+               </div>
+               <div className="col-md-4">
+               <PostCard />
+               </div>
+               <div className="col-md-4">
+               <PostCard />
+               </div>
+            </div>
         </div>
     )
 }
