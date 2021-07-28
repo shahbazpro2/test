23b0c6f35eaf34c225f91137576cddd0c3c1ba6d
shahbaz <shahbazrock0321@gmail.com>
2021-07-28 20:19:15 +0500

    Import redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import RootReducer from './redux/reducers/RootReducer';
 import reportWebVitals from './reportWebVitals';
-const store = createStore(RootReducer)
+const store = createStore(RootReducer,composeWithDevTools())
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
