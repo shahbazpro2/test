d404602933cc6be1ff7e3d8ed8a55f1877452348
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 20:09:30 +0500

    remove unnessary action file

--- src/redux/actions/RemoveSinglePost.js
+++
@@ -1,9 +0,0 @@
-import * as types from './types'
-
-const removeSinglePost = () => {
-    return (dispatch) => {
-        dispatch({ type: types.REMOVE_SINGLE_POST})
-    };
-};
-
-export default removeSinglePost
