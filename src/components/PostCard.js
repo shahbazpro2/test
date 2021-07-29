61af7bd74c38cf21823fd78a0a36907e28dc734a
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 17:46:13 +0500

    Add edit url

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const PostCard = ({ post,removePostFun }) => {
+const PostCard = ({ post, removePostFun }) => {
   return (
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">{`${post.id}-${post.title}`}</h5>
         <p className="card-text">{post.body}</p>
         <div className="d-flex justify-content-evenly">
-        <Link to={`/post/${post.id}`} className="btn btn-sm btn-primary">See more</Link>
-        <Link to="/" className="btn btn-sm btn-success">Edit</Link>
-        <button className="btn btn-sm btn-danger" onClick={()=>removePostFun(post.id)}>Remove</button>
+          <Link to={`/post/${post.id}`} className="btn btn-sm btn-primary">See more</Link>
+          <Link to={`/edit/${post.id}`} className="btn btn-sm btn-success">Edit</Link>
+          <button className="btn btn-sm btn-danger" onClick={() => removePostFun(post.id)}>Remove</button>
         </div>
-        
+
       </div>
     </div>
   )
