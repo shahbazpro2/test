6c9e70f5f7bc8f7401e724f9fd097d21ed8f34d4
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 10:59:10 +0500

    dispatch get all posts action in useEffect

--- src/pages/PostsList.js
+++ src/pages/PostsList.js
@@ -1,11 +1,15 @@
-import React,{useState} from 'react'
-import { useSelector } from 'react-redux'
+import React,{useState,useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import PostCard from '../components/PostCard'
+import getAllPosts from '../redux/actions/GetAllPosts';
 
 const PostsList = () => {
     const state = useSelector(state => state.allPosts)
+    const dispatch=useDispatch()
     const [posts,setPosts]=useState(state.posts)
-    
+    useEffect(() => {
+       dispatch(getAllPosts())
+    }, [])
     return (
         <div className="container">
             <div className="row">
