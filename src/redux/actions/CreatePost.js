86a2966b41762869a72801304e324f05651d4d48
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 16:40:15 +0500

    remove unnessary code

--- src/redux/actions/CreatePost.js
+++ src/redux/actions/CreatePost.js
@@ -2,9 +2,8 @@ import * as types from './types'
 
 const createPost = (values, posts) => {
     return (dispatch) => {
-        dispatch({ type: types.CREATE_POST });
+        dispatch({ type: types.GET_ALL_POSTS });
         const newPosts = posts.push({ id: posts.length + 1, ...values })
-        dispatch({ type: types.CREATE_POST_SUCCESS })
         dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts: newPosts })
     };
 };
