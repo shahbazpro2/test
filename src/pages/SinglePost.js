f12382c66609a1d5ec10aa73d4303e7cbf703b16
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 13:51:01 +0500

    make data dynamic

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,22 +1,22 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import getSinglePost from '../redux/actions/GetSinglePost'
 
 const SinglePost = () => {
-    const singlePost=useSelector(state=>state.singlePost)
+    const singlePost = useSelector(state => state.singlePost)
     let { id } = useParams();
-    const dispatch=useDispatch()
+    const dispatch = useDispatch()
     useEffect(() => {
-       dispatch(getSinglePost(id))
+        dispatch(getSinglePost(id))
     }, [])
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-10">
-                    <h1>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h1>
+                    <h1>{singlePost.post.title}</h1>
                     <p className="mt-2">
-                        quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto
+                        {singlePost.post.body}
                     </p>
                 </div>
             </div>
