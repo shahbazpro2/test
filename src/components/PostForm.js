c803394afc3d6012496cfdb899685d73bf9a1c68
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 15:20:53 +0500

    form layout

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 const PostForm = () => {
     return (
-        <div>
-            
-        </div>
+        <form className="row g-3 needs-validation" novalidate>
+            <div className="col-md-4">
+                <label for="title" className="form-label">Title</label>
+                <input type="text" className="form-control" id="title" value="title" required />
+            </div>
+            <div className="col-md-4">
+                <label for="body" className="form-label">Body</label>
+                <input type="text" className="form-control" id="body" value="title" required />
+            </div>
+            <div className="col-12">
+                <button className="btn btn-primary" type="submit">Submit</button>
+            </div>
+        </form>
     )
 }
 
