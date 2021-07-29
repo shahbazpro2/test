16fbd8ec1bad39b4afed86acd41566871f858627
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 11:33:57 +0500

    Dynamic data received from data props

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const PostCard = () => {
-    return (
-        <div className="card">
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="/" className="btn btn-primary">see more</a>
-  </div>
-</div>
-    )
+const PostCard = ({ data }) => {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{`${data.id}-${data.title}`}</h5>
+        <p className="card-text">{data.body}</p>
+        <a href="/" className="btn btn-primary">see more</a>
+      </div>
+    </div>
+  )
 }
 
 export default PostCard
