039b8e0670186739db7237cb5e87f51532ac7e6e
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 15:01:52 +0500

    add create post button

--- src/pages/PostsList.js
+++ src/pages/PostsList.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import PostCard from '../components/PostCard'
 import getAllPosts from '../redux/actions/GetAllPosts';
 import removePost from './../redux/actions/RemovePost';
+import { Link } from 'react-router-dom';
 
 const PostsList = () => {
     const allPosts = useSelector(state => state.allPosts)
@@ -11,14 +12,15 @@ const PostsList = () => {
         dispatch(getAllPosts())
     }, [])
 
-    const removePostFun=(id)=>{
-        const confirm=window.confirm("Are you sure you want to delete?")
-        if(confirm===true){
-            dispatch(removePost(id,allPosts.posts))
+    const removePostFun = (id) => {
+        const confirm = window.confirm("Are you sure you want to delete?")
+        if (confirm === true) {
+            dispatch(removePost(id, allPosts.posts))
         }
     }
     return (
         <div className="container">
+            <Link to="/post/create" className="btn btn-primary mb-3">Create a new post</Link>
             <div className="row">
                 {allPosts.posts.map((post) => (
                     <div key={post.id} className="col-md-4 my-2">
