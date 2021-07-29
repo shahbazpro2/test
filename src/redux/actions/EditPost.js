eec3805ad55e08914486b2a83dc3bcb686c293a5
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 19:22:36 +0500

    change variable name

--- src/redux/actions/EditPost.js
+++ src/redux/actions/EditPost.js
@@ -3,12 +3,12 @@ import * as types from './types'
 const editPost = (values, posts) => {
     return (dispatch) => {
         dispatch({ type: types.GET_ALL_POSTS });
-        let newPosts = posts.map(p => {
-            if (p.id === values.id) {
-                delete p['title']
-                delete p['body']
-                return { ...p, ...values }
-            } else return p
+        let newPosts = posts.map(post => {
+            if (post.id === values.id) {
+                delete post['title']
+                delete post['body']
+                return { ...post, ...values }
+            } else return post
         })
         setTimeout(() => {
             dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts: newPosts })
