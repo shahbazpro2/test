209f975eeae0e6fac1bab943a7124309ed3de685
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 13:34:55 +0500

    fix wrong type names

--- src/redux/reducers/GetSinglePostReducer.js
+++ src/redux/reducers/GetSinglePostReducer.js
@@ -7,18 +7,18 @@ const initialAllPostState = {
 
 const getSinglePostReducer = (state = initialAllPostState, action) => {
     switch (action.type) {
-        case types.GET_SINGLE_POSTS:
+        case types.GET_SINGLE_POST:
             return {
                 ...state,
                 loading: true
             }
-        case types.GET_SINGLE_POSTS_SUCCESS:
+        case types.GET_SINGLE_POST_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 post: action.post
             }
-        case types.GET_SINGLE_POSTS_FAILURE:
+        case types.GET_SINGLE_POST_FAILURE:
             return {
                 ...state,
                 loading: false,
