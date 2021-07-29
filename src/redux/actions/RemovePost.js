bd061ca5c329003353380b47dab3d8d65c7e0624
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 14:29:19 +0500

    filter and remove post and dispatch remaining posts

---
+++ src/redux/actions/RemovePost.js
@@ -0,0 +1,11 @@
+import * as types from './types'
+
+const getSinglePost = (id, posts) => {
+    return (dispatch) => {
+        dispatch({ type: types.REMOVE_POST });
+        const filter = posts.filter(p => p.id !== id)
+        dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts:filter })
+    };
+};
+
+export default getSinglePost
