04ceaa2abc3a72aea74ea109b0021f59475159d7
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 13:31:51 +0500

    make a setup for initialize and set updated state according to actions

---
+++ src/redux/reducers/GetSinglePostReducer.js
@@ -0,0 +1,32 @@
+import * as types from '../actions/types'
+const initialAllPostState = {
+    post: {},
+    loading: false,
+    error: null
+}
+
+const getSinglePostReducer = (state = initialAllPostState, action) => {
+    switch (action.type) {
+        case types.GET_SINGLE_POSTS:
+            return {
+                ...state,
+                loading: true
+            }
+        case types.GET_SINGLE_POSTS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                post: action.post
+            }
+        case types.GET_SINGLE_POSTS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.err
+            }
+        default:
+            return state
+    }
+}
+
+export default getSinglePostReducer
