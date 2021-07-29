a668826b44c2ea6fc5a975a1bb1331edab959f8d
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 11:18:58 +0500

    get exact data array from axios response

--- src/redux/actions/GetAllPosts.js
+++ src/redux/actions/GetAllPosts.js
@@ -6,7 +6,7 @@ const getAllPosts = () => {
     return (dispatch) => { 
       dispatch({ type: types.GET_ALL_POSTS });
       return axios.get(`${apiUrl}/posts`).then(  
-        posts => dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts }),
+        posts =>dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts:posts.data }),
         err => dispatch({ type: types.GET_ALL_POSTS_FAILURE, err })
       );
     };
