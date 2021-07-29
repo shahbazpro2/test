4cb29ccf24ac6ae5ce8f174fddb3154eb46ce8a6
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 17:20:02 +0500

    dispatch create post action and navigate to postlist page

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PostForm from '../components/PostForm'
+import { useDispatch, useSelector } from 'react-redux';
+import createPost from '../redux/actions/CreatePost';
+import { useHistory } from 'react-router-dom';
 
 const CreatePost = () => {
-    const onSubmitFun=(values)=>{
-        console.log(values)
+    const allPosts = useSelector(state => state.allPosts)
+    const dispatch = useDispatch()
+    let history = useHistory();
+    const onSubmitFun = (values) => {
+        dispatch(createPost(values, allPosts.posts))
+        history.push('/')
     }
     return (
         <div>
-            <PostForm onSubmitFun={onSubmitFun} title="create post" />
+            <PostForm onSubmitFun={onSubmitFun} title="Create post" />
         </div>
     )
 }
