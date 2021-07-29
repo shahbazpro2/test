6e2a4b23f2ca1d18a4d2b26f9de1d53549c8a5bf
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 15:31:34 +0500

    change layout

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -2,19 +2,26 @@ import React from 'react'
 
 const PostForm = () => {
     return (
-        <form className="row g-3 needs-validation" novalidate>
-            <div className="col-md-4">
-                <label for="title" className="form-label">Title</label>
-                <input type="text" className="form-control" id="title" value="title" required />
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-7">
+                    <form className="g-3 needs-validation" novalidate>
+                        <div className="py-2">
+                            <label for="title" className="form-label">Title</label>
+                            <input type="text" className="form-control" id="title" required />
+                        </div>
+                        <div className="py-2">
+                            <label for="body" className="form-label">Body</label>
+                            <textarea class="form-control" id="body" required></textarea>
+                        </div>
+                        <div className="py-2">
+                            <button className="btn btn-primary" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className="col-md-4">
-                <label for="body" className="form-label">Body</label>
-                <input type="text" className="form-control" id="body" value="title" required />
-            </div>
-            <div className="col-12">
-                <button className="btn btn-primary" type="submit">Submit</button>
-            </div>
-        </form>
+        </div>
+
     )
 }
 
