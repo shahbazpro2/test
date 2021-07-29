446e5a182e17cac6149edd8362444a1bfbd9fc9a
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 11:30:31 +0500

    map all posts received from redux

--- src/pages/PostsList.js
+++ src/pages/PostsList.js
@@ -1,26 +1,22 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import PostCard from '../components/PostCard'
 import getAllPosts from '../redux/actions/GetAllPosts';
 
 const PostsList = () => {
     const state = useSelector(state => state.allPosts)
-    const dispatch=useDispatch()
+    const dispatch = useDispatch()
     useEffect(() => {
-       dispatch(getAllPosts())
+        dispatch(getAllPosts())
     }, [])
     return (
         <div className="container">
             <div className="row">
-               <div className="col-md-4">
-                    <PostCard />
-               </div>
-               <div className="col-md-4">
-               <PostCard />
-               </div>
-               <div className="col-md-4">
-               <PostCard />
-               </div>
+                {state.posts.map((post) => (
+                    <div key={post.id} className="col-md-4 my-2">
+                        <PostCard data={post} />
+                    </div>
+                ))}
             </div>
         </div>
     )
