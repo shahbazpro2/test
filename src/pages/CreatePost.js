417ca8b21f7c9e533a4d351153bfe2fdf304c79b
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 15:21:54 +0500

    Add route for create post

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import PostForm from '../components/PostForm'
 
 const CreatePost = () => {
     return (
         <div>
-            
+            <PostForm />
         </div>
     )
 }
