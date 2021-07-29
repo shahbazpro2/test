380f72987f7780ee0bdc1f9524f705ec8c890425
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 13:38:49 +0500

    Change data props to post props for better readable

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const PostCard = ({ data }) => {
+const PostCard = ({ post }) => {
   return (
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">{`${data.id}-${data.title}`}</h5>
-        <p className="card-text">{data.body}</p>
+        <h5 className="card-title">{`${post.id}-${post.title}`}</h5>
+        <p className="card-text">{post.body}</p>
         <div className="d-flex justify-content-evenly">
-        <Link href={`/post/${data.id}`} className="btn btn-sm btn-primary">See more</Link>
+        <Link href={`/post/${post.id}`} className="btn btn-sm btn-primary">See more</Link>
         <Link href="/" className="btn btn-sm btn-success">Edit</Link>
         <button className="btn btn-sm btn-danger">Remove</button>
         </div>
