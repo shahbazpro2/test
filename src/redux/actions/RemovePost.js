f7a9160a5a7193d15c5fb4d257cf598abdf2860f
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 16:37:26 +0500

    remove unnessary dispatch

--- src/redux/actions/RemovePost.js
+++ src/redux/actions/RemovePost.js
@@ -2,7 +2,6 @@ import * as types from './types'
 
 const removePost = (id, posts) => {
     return (dispatch) => {
-        dispatch({ type: types.REMOVE_POST });
         const filter = posts.filter(p => p.id !== id)
         dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts:filter })
     };
