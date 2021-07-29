5f2244e4f8d2965a9a6fbc2c52c094d0381887b7
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 12:41:41 +0500

    layout with dummy data

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -3,7 +3,10 @@ import React from 'react'
 const SinglePost = () => {
     return (
         <div>
-            
+            <h1>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h1>
+            <div className="mt-2">
+                quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto
+            </div>
         </div>
     )
 }
