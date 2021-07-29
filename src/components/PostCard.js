c98efb5a2c44b12d66613d9b8eebc8282b3d289a
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 13:40:20 +0500

    fix href to to

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -8,7 +8,7 @@ const PostCard = ({ post }) => {
         <h5 className="card-title">{`${post.id}-${post.title}`}</h5>
         <p className="card-text">{post.body}</p>
         <div className="d-flex justify-content-evenly">
-        <Link href={`/post/${post.id}`} className="btn btn-sm btn-primary">See more</Link>
+        <Link to={`/post/${post.id}`} className="btn btn-sm btn-primary">See more</Link>
         <Link href="/" className="btn btn-sm btn-success">Edit</Link>
         <button className="btn btn-sm btn-danger">Remove</button>
         </div>
