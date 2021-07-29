802e7ef7b76957d930992c19e4b71a3d4a846d3c
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 16:54:56 +0500

    fix push issue

--- src/redux/actions/CreatePost.js
+++ src/redux/actions/CreatePost.js
@@ -3,8 +3,8 @@ import * as types from './types'
 const createPost = (values, posts) => {
     return (dispatch) => {
         dispatch({ type: types.GET_ALL_POSTS });
-        const newPosts = posts.push({ id: posts.length + 1, ...values })
-        dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts: newPosts })
+        posts.push({ id: posts.length + 1, ...values })
+        dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts: posts })
     };
 };
 
