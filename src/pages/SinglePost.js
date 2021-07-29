ce9cf3ffb8cede0bddf03ffdb5de5948574139e0
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 13:48:05 +0500

    dispatch getSinglePost action

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React,{useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import getSinglePost from '../redux/actions/GetSinglePost'
 
 const SinglePost = () => {
     const singlePost=useSelector(state=>state.singlePost)
-    
+    let { id } = useParams();
+    const dispatch=useDispatch()
+    useEffect(() => {
+       dispatch(getSinglePost(id))
+    }, [])
     return (
         <div className="container">
             <div className="row justify-content-center">
