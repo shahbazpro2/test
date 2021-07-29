029979d0f4dd0b4caee2c14a73c29ee003bb8fe3
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 12:44:03 +0500

    add route single post

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PostsList from './pages/PostsList';
+import SinglePost from './pages/SinglePost';
 function App() {
   return (
     <div className="App pt-5">
       <Router>
         <Switch>
           <Route exact path="/" component={PostsList} />
+          <Route exact path="/post/:id" component={SinglePost} />
         </Switch>
       </Router>
 
