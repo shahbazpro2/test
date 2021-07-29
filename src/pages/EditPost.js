fe42cce08a493be16539457051ff6589387e147c
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 17:50:28 +0500

    dispatch single post for get data

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React,{useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import PostForm from './../components/PostForm';
+import { useParams } from 'react-router-dom';
+import getSinglePost from '../redux/actions/GetSinglePost';
 
 const EditPost = () => {
     const allPosts = useSelector(state => state.allPosts)
+    let { id } = useParams();
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getSinglePost(id))
+    }, [])
     const onSubmitFun = (values) => {
         console.log(values)
     }
