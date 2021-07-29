8d6e4e8d7b783f495c54eaedd5b8603b96896b6a
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 14:38:32 +0500

    Make remove post function and pass this to postCard

--- src/pages/PostsList.js
+++ src/pages/PostsList.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import PostCard from '../components/PostCard'
 import getAllPosts from '../redux/actions/GetAllPosts';
+import removePost from './../redux/actions/RemovePost';
 
 const PostsList = () => {
     const allPosts = useSelector(state => state.allPosts)
@@ -9,12 +10,19 @@ const PostsList = () => {
     useEffect(() => {
         dispatch(getAllPosts())
     }, [])
+
+    const removePostFun=(id)=>{
+        const confirm=window.confirm("Are you sure you want to delete?")
+        if(confirm===true){
+            dispatch(removePost(id))
+        }
+    }
     return (
         <div className="container">
             <div className="row">
                 {allPosts.posts.map((post) => (
                     <div key={post.id} className="col-md-4 my-2">
-                        <PostCard post={post} />
+                        <PostCard post={post} removePostFun={removePostFun} />
                     </div>
                 ))}
             </div>
