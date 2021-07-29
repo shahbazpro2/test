4e6296af63b68e74849147903687f19ba50ae526
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 13:38:01 +0500

    change a tag to Link tag and update the path of "see more"

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PostCard = ({ data }) => {
   return (
@@ -7,8 +8,8 @@ const PostCard = ({ data }) => {
         <h5 className="card-title">{`${data.id}-${data.title}`}</h5>
         <p className="card-text">{data.body}</p>
         <div className="d-flex justify-content-evenly">
-        <a href="/" className="btn btn-sm btn-primary">See more</a>
-        <a href="/" className="btn btn-sm btn-success">Edit</a>
+        <Link href={`/post/${data.id}`} className="btn btn-sm btn-primary">See more</Link>
+        <Link href="/" className="btn btn-sm btn-success">Edit</Link>
         <button className="btn btn-sm btn-danger">Remove</button>
         </div>
         
