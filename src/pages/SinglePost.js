9996bc786ee9f19503567a4d709047e2ce7d4bb2
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 13:42:23 +0500

    get singlePost from redux

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const SinglePost = () => {
+    const singlePost=useSelector(state=>state.singlePost)
+    
     return (
         <div className="container">
             <div className="row justify-content-center">
