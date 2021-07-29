258d3c4b8a9e15f3c8b3d98f69bb286a1bc316ea
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 17:01:21 +0500

    fix posts push

--- src/redux/actions/CreatePost.js
+++ src/redux/actions/CreatePost.js
@@ -3,8 +3,9 @@ import * as types from './types'
 const createPost = (values, posts) => {
     return (dispatch) => {
         dispatch({ type: types.GET_ALL_POSTS });
-        posts.push({ id: posts.length + 1, ...values })
-        dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts: posts })
+        let newPosts=posts
+        newPosts.push({ id: posts.length + 1, ...values })
+        dispatch({ type: types.GET_ALL_POSTS_SUCCESS, posts: newPosts })
     };
 };
 
