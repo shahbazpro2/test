f7cb0cca14c017850964ef798976742e3f7ceb18
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2021-07-29 18:54:28 +0500

    add spinner before fetching all posts

--- src/pages/PostsList.js
+++ src/pages/PostsList.js
@@ -17,11 +17,14 @@ const PostsList = () => {
         <div className="container">
             <Link to="/create" className="btn btn-primary mb-3">Create a new post</Link>
             <div className="row">
-                {allPosts.posts.map((post) => (
-                    <div key={post.id} className="col-md-4 my-2">
-                        <PostCard post={post} removePostFun={removePostFun} />
-                    </div>
-                )).reverse()}
+                {allPosts.loading === true ? <div className="spinner-border text-primary" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div> :
+                    allPosts.posts.map((post) => (
+                        <div key={post.id} className="col-md-4 my-2">
+                            <PostCard post={post} removePostFun={removePostFun} />
+                        </div>
+                    )).reverse()}
             </div>
         </div>
     )
